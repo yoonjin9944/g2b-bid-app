44d8a3a1306c524d0c80babe0f7174a680b6a80c
ChoiYoonjin <yoonjin9944@gmail.com>
2026-06-09 13:52:36 +0900

    앱 배포 테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.g2b.bidapp"
         minSdk = 33
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "com.g2b.bidapp.HiltTestRunner"
